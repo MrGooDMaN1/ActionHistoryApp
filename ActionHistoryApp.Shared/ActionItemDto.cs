3d22c35710f9cd55052baf223ef7918272bd893e
k.kovalenko <k.kovalenko@itv34.ru>
2026-02-18 15:56:58 +0300

    Добавил валидацию

--- ActionHistoryApp.Shared/ActionItemDto.cs
+++ ActionHistoryApp.Shared/ActionItemDto.cs
@@ -1,10 +1,18 @@
-﻿namespace ActionHistoryApp.Shared
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ActionHistoryApp.Shared
 {
     public class ActionItemDto
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Выбирите название базы")]
         public string DataBaseName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Выбирите имя исполнителя")]
         public string WhoDid { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Введите описание")]
         public string WhatDid { get; set; } = string.Empty;
         public DateTime WhenDid { get; set; }
     }
