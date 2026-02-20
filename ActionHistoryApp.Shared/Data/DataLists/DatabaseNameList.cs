53d0d6a854aa053e3d5f0bd187b824ad888132a1
k.kovalenko <k.kovalenko@itv34.ru>
2026-02-20 11:30:56 +0300

    Добавил название баз и подправил внешний вид формы

--- ActionHistoryApp.Shared/Data/DataLists/DatabaseNameList.cs
+++ ActionHistoryApp.Shared/Data/DataLists/DatabaseNameList.cs
@@ -8,7 +8,30 @@ namespace ActionHistoryApp.Shared.Data.DataLists
     {
         public static readonly string[] databaseName = new[]
         {
-            "Sheigerevich", "1112", "SchoolCafe", "Pspion", "IT", "EcoBiz", "Kredo"
+            "Sheigerevich", 
+            "1112", 
+            "SchoolCafe", 
+            "Pspion", 
+            "IT", 
+            "EcoBiz", 
+            "Kredo", 
+            "Losh", 
+            "Maslova", 
+            "Mstandart", 
+            "Platforma15_erp", 
+            "Shabli", 
+            "ASP",
+            "САВ",
+            "Fedunishina",
+            "Batrakova",
+            "VivoMarket",
+            "Алеко 2024",
+            "ВолгаВиво2024",
+            "FoodKonsalt_2024",
+            "Bis",
+            "Ivanova-SchasteEst",
+            "Financier",
+            "Buh_Zhilina",
         };
     }
 }
